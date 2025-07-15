7c1ebca2032b3cce7368d9b4904d0575b1b1cd92
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-07-14 22:52:22 -0400

    Shekar, 7-15-63

--- theNames.js
+++ theNames.js
@@ -323,7 +323,7 @@ export const theNames = [
     firstName: "Shekar",
     lastName: "I.",
     birthMonth: "July",
-    birthDay: 14,
+    birthDay: 15,
     birthYear: 1963,
     passedAway: null,
     comment: null,
